869e603c1b2837ad9b5362201da67a31d5c4f2f4
Aneta A. Ozvat <anetaozvatova@gmail.com>
2018-10-02 09:39:04 +0200

    Vytvorenie .h suboru s funkciami.

---
+++ counter.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+// funkcia na pocitanie pismen
+int count_letters(string input)
+{
+	int n = 0;
+
+	for (int i = 0; i < input.size(); i++) {
+		if (!isspace(input[i])) {
+			n++;
+		}
+	}
+
+	return n;
+}
+
+// funkcia na pocitanie slov
+int count_words(string input)
+{
+	int n = 1;
+
+	for (int i = 0; i < input.size(); i++) {
+		// ak je viac medzier nak sa rataju ako edna
+		if (input[i] == ' ' && input[i + 1] != ' ') {
+			n++;
+		}
+	}
+
+	// ak je "abc efg ", aby bolo n = 2 a ni n = 3
+	if (input[input.size() - 1] == ' ') {
+		n--;
+	}
+
+	return n;
+}
+
+// funkcia na pocitanie riadkov
+//int count_lines(string input)
+//{
+
+	//return n;
+//}
--- main.cpp
+++ main.cpp
@@ -6,42 +6,9 @@
 #include <fstream>
 #include <algorithm>
 #include <iterator>
+#include "counter.h"
 
 using namespace std;
-
-// funkcia na pocitanie pismen
-int count_letters(string input)
-{
-	int n = 0;
-
-	for (int i = 0; i < input.size(); i++) {
-		if (!isspace(input[i])) {
-			n++;
-		}
-	}
-
-	return n;
-}
-
-// funkcia na pocitanie slov
-int count_words(string input)
-{
-	int n = 1;
-
-	for (int i = 0; i < input.size(); i++) {
-		// ak je viac medzier nak sa rataju ako edna
-		if (input[i] == ' ' && input[i + 1] != ' ') {
-			n++;
-		}
-	}
-
-	// ak je "abc efg ", aby bolo n = 2 a ni n = 3
-	if (input[input.size() - 1] == ' ') {
-		n--;
-	}
-
-	return n;
-}
 
 int main(int argc, char **argv)
 {
@@ -110,7 +77,7 @@ int main(int argc, char **argv)
 		// ak nie je vneseny vstupny subor
 		else {
 			cout << "Vneste text (vstup ukoncite stlacenim ` + Enter): " << endl;
-
+			
 			char ch;
 			int n = 1;
 
@@ -120,7 +87,6 @@ int main(int argc, char **argv)
 					n++;
 				}
 			}
-
 			cout << "Pocet riadkov v texte je: " << n << endl;
 		};
 	}
